5e951b2c5c6852559af5c4c3b055538d58e13d9f
Hiag0liveira <hiagophd@gmail.com>
2024-03-02 01:04:11 -0300

    test

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -1,31 +1,9 @@
-import { createContext, useState, useEffect } from "react";
-import { userLogged } from "../services/userServices";
-import { Loading } from "../components/Loading/Loading";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
 export default function UserProvider({ children }) {
   const [user, setUser] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-
-  async function loadUser() {
-    setIsLoading(true);
-    try {
-      const response = await userLogged();
-      setUser(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-    setIsLoading(false);
-  }
-
-  useEffect(() => {
-    loadUser();
-  }, []);
-
-  if (isLoading) {
-    return <Loading />;
-  }
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
